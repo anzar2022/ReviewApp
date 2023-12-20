3386492bbf0340b1ab48448f606fe17d8d654b8d
Anzar Ansari <anzar.ansari@wonderbiz.in>
2023-12-20 11:02:58 +0530

    Added Scoped

--- ReviewApp.ReviewTaskApi/Program.cs
+++ ReviewApp.ReviewTaskApi/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using ReviewApp.Data;
+using ReviewApp.IRepositories;
+using ReviewApp.IServices;
+using ReviewApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,16 @@ var connectionString = configuratoin.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ReviewAppDbContext>(options => options.UseSqlServer(connectionString, b => b.MigrationsAssembly("ReviewApp.ReviewTaskApi")));
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IQuarterService, QuarterService>();
+builder.Services.AddScoped<IReviewTaskService, ReviewTaskService>();
+builder.Services.AddScoped<IStatusService, StatusService>();
+
+
+builder.Services.AddScoped<IQuarterRepository, IQuarterRepository>();
+builder.Services.AddScoped<IReviewTaskRepository, IReviewTaskRepository>();
+builder.Services.AddScoped<IStatusRepository, IStatusRepository>();
+
 
 var app = builder.Build();
 app.MapControllers();
