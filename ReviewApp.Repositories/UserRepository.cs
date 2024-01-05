cf3d4301ada90fd94a83b5f0a4a56cd7342ab44b
Anzar Ansari <anzar.ansari@wonderbiz.in>
2024-01-05 10:49:46 +0530

    Code clean

--- ReviewApp.Repositories/UserRepository.cs
+++ ReviewApp.Repositories/UserRepository.cs
@@ -2,11 +2,6 @@
 using ReviewApp.Data;
 using ReviewApp.IRepositories;
 using ReviewApp.Model;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace ReviewApp.Repositories
 {
@@ -16,7 +11,7 @@ namespace ReviewApp.Repositories
         {
         }
 
-        
+
 
         //public UserRepository(DbContext context) : base(context)
         //{
--- ReviewApp.ReviewTaskApi/Program.cs
+++ ReviewApp.ReviewTaskApi/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using ReviewApp.Data;
@@ -64,7 +62,7 @@ builder.Services.Configure<RouteOptions>(options =>
 
 
 builder.Services.AddAuthentication(options =>
-{ 
+{
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(options =>
